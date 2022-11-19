46e23d211082029f88d7547b739c08f57e587bbe
ETERNAL-U9 <eternalwind118@gmail.com>
2022-11-18 20:24:17 -0500

    Initial core implementation with improvement TODOs

---
+++ src/main/java/useCases/RegisterResponseModel.java
@@ -0,0 +1,29 @@
+package useCases;
+
+public class RegisterResponseModel {
+
+    String login;
+    String creationTime;
+
+    public RegisterResponseModel(String login, String creationTime) {
+        this.login = login;
+        this.creationTime = creationTime;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+
+}
