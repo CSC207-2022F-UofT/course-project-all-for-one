87b74107ea635730e1893e507954150a824ba3e5
xavierquan <xavier.quan@mail.utoronto.ca>
2022-11-18 20:26:32 -0500

    trade6.0

--- src/main/java/gateway/OrderDsGateway.java
+++ src/main/java/gateway/OrderDsGateway.java
@@ -1,11 +1,11 @@
-package gateway;
-
-import use_case.OrderDsRequestModel;
-
-import java.util.Map;
-
-public interface OrderDsGateway {
-    boolean existsByName(String identifier);
-
-    void save(String orderPath, Map<>OrderDsRequestModel requestModel);
-}
+//package gateway;
+//
+//import use_case.OrderDsRequestModel;
+//
+//import java.util.Map;
+//
+//public interface OrderDsGateway {
+//    boolean existsByName(String identifier);
+//
+//    void save(String orderPath, Map<>OrderDsRequestModel requestModel);
+//}
