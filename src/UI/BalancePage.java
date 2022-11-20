3ad8c8df126b8c23e895d188ae21fa7d826dd50d
Wang-Benjamin <yakunuoft.wang@mail.utoronto.ca>
2022-11-20 14:52:38 -0500

    Wallet

--- src/UI/BalancePage.java
+++ src/UI/BalancePage.java
@@ -4,6 +4,6 @@ import javax.swing.*;
 
 public class BalancePage extends JPanel{
     public BalancePage(double balance) {
-        JLabel money = new JLabel("Balance:" + balance, SwingConstants.CENTER);
+        JLabel money = new JLabel("Balance: " + balance, SwingConstants.CENTER);
     }
 }
