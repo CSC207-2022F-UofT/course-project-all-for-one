8fa49c2c220762ff719581f29d7d07f504aae8ba
Wang-Benjamin <yakunuoft.wang@mail.utoronto.ca>
2022-11-28 17:39:48 -0500

    Purchase History

--- src/main/java/gateway/FilePurchase.java
+++ src/main/java/gateway/FilePurchase.java
@@ -8,38 +8,31 @@ public class FilePurchase implements PurchaseGateway {
 
     private final File csvFile;
 
-    private final Map<String, Integer> headers = new LinkedHashMap<>();
-
     private final Map<String, PurchaseRequestModel> accounts = new HashMap<>();
 
     public FilePurchase(String csvPath) throws IOException{
 
         csvFile = new File(csvPath);
 
-        headers.put("username", 0);
-        for (int i = 1; i < csvFile.length(); i++) {
-            headers.put("creation_time" + i, i);
 
-            if (csvFile.length() == 0) {
-                save();
-            } else {
+        BufferedReader reader = new BufferedReader(new FileReader(csvFile));
+        reader.readLine();
 
-                BufferedReader reader = new BufferedReader(new FileReader(csvFile));
-                reader.readLine();
-
-                String row;
-                while ((row = reader.readLine()) != null) {
-                    String[] col = row.split(",");
-                    String username = String.valueOf(col[headers.get("username")]);
-                    List<String> creation_times = new ArrayList<>();
-                    creation_times.add(col[headers.get("creation_time" + i)]);
-                    PurchaseRequestModel purchase = new PurchaseRequestModel(username, creation_times);
-                    accounts.put(username, purchase);
-                }
-
-                reader.close();
+        String row;
+        while ((row = reader.readLine()) != null) {
+            String[] col = row.split(",");
+            String username = String.valueOf(col[0]);
+            List<String> creation_times = new ArrayList<>();
+            for (int i = 1; i < col.length; i++ ){
+                creation_times.add(col[i]);
             }
+            PurchaseRequestModel purchase = new PurchaseRequestModel(username, creation_times);
+            accounts.put(username, purchase);
         }
+
+        reader.close();
+
+
     }
 
     @Override
@@ -52,12 +45,13 @@ public class FilePurchase implements PurchaseGateway {
         BufferedWriter writer;
         try {
             writer = new BufferedWriter(new FileWriter(csvFile));
-            writer.write(String.join(",", headers.keySet()));
-            writer.newLine();
 
             for (PurchaseRequestModel purchase : accounts.values()) {
-                Long n = csvFile.length() - 1;
-                String line = String.format("%s".repeat(n.intValue()), purchase.getCreationTimes());
+                String line = "";
+                line += purchase.getUsername();
+                for (String creationTime: purchase.getCreationTimes()){
+                    line += "," + creationTime ;
+                }
                 writer.write(line);
                 writer.newLine();
             }
