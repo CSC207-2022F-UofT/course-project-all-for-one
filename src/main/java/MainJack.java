9f88bd0e21113e2513e8615403d79c47dd1d145f
jackjin <jiekejindsg@163.com>
2022-12-02 21:38:57 -0500

    connect with Post Page

--- src/main/java/MainJack.java
+++ src/main/java/MainJack.java
@@ -1,3 +1,4 @@
+import application_business_rules_layer.messageUseCases.MessageResponseModel;
 import framworks_drivers_layer.views.Board;
 import framworks_drivers_layer.dataAccess.FileMessage;
 import application_business_rules_layer.messageUseCases.MessageDsGateway;
@@ -9,10 +10,11 @@ import java.io.FileNotFoundException;
 public class MainJack {
     public static void main(String[] args) throws FileNotFoundException {
 
+        String boardName = "iphone";
 
         MessageDsGateway Messages = new FileMessage("./MessageBoard.csv");
 
-        JFrame board = new Board(Messages, "iphone");
+        JFrame board = new Board(Messages, boardName);
         board.setVisible(true);
 
     }
