eae9200066e9127a33abd3b46c270dd70a63ea54
Kevin You <kevin.you0726@gmail.com>
2022-12-03 20:28:15 -0500

    Post all set

--- src/main/java/application_business_rules_layer/postUseCases/PostInputBoundary.java
+++ src/main/java/application_business_rules_layer/postUseCases/PostInputBoundary.java
@@ -1,6 +1,11 @@
 package application_business_rules_layer.postUseCases;
 
 public interface PostInputBoundary {
+    /**
+     *
+     * @param requestModel the PostRequestModel that serves as the input needed processing the post creation
+     * @return the PostResponseModel object that is the output after processing the post creation
+     */
     PostResponseModel create(PostRequestModel requestModel);
 
     void deletion(PostRequestModel requestModel);
