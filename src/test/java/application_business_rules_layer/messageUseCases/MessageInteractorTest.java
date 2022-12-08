bf6d095571775801a2f26cfa697bfa66217b9bee
jackjin <jiekejindsg@163.com>
2022-12-07 23:57:04 -0500

    test

--- src/test/java/application_business_rules_layer/messageUseCases/MessageInteractorTest.java
+++ src/test/java/application_business_rules_layer/messageUseCases/MessageInteractorTest.java
@@ -1,22 +1,31 @@
 package application_business_rules_layer.messageUseCases;
 
+import Interface_adapters_layer.presenter.MessagePresenter;
+import Interface_adapters_layer.presenter.MessageResponseFormatter;
+import enterprise_business_rules_layer.messageEntities.MessageBoard;
+import framworks_drivers_layer.dataAccess.FileMessage;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.*;
 
 public class MessageInteractorTest {
+    static int n=3;
 
-    @Before
-    public void setUp() throws Exception {
-    }
+   MessageRequestModel requestModel = new MessageRequestModel("I like this", "Jack", "iphone");
+   MessageDsGateway dsGateway = new FileMessage("./MessageTest.csv");
+   MessagePresenter presenter = new MessageResponseFormatter();
+   MessageInteractor interactor = new MessageInteractor(dsGateway, presenter);
 
-    @After
-    public void tearDown() throws Exception {
-    }
+
 
     @Test
     public void create() {
+        MessageResponseModel responseModel = interactor.create(requestModel);
+        assertEquals(responseModel.getMessageList().size(), n);
+        n += 1;
+
     }
+
 }
